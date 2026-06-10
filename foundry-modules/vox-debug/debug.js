2fb50b9ec9f1976930158fde48f9c9d7172b786b
Your Name <your.email@example.com>
2026-06-10 15:41:48 -0600

    style: Move VOX DEBUG to background (remove blue overlay)

--- foundry-modules/vox-debug/debug.js
+++ foundry-modules/vox-debug/debug.js
@@ -1,5 +1,7 @@
-console.log("🚨 VOX DEBUG LOADED");
+console.log("🚨 VOX DEBUG LOADED (Background Mode)");
+/*
 const div = document.createElement("div");
 div.style = "position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); width: 200px; height: 200px; background: blue; color: white; z-index: 100000; display: flex; align-items: center; justify-content: center; font-size: 20px; font-weight: bold;";
 div.innerText = "VOX DEBUG ACTIVE";
 document.body.appendChild(div);
+*/
